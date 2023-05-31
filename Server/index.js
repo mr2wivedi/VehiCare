029438db06474f90949e52bbe0dc8222b5679ef7
Shivansh <dwishwid05@gmail.com>
2023-06-01 00:00:26 +0530

    final commit

---
+++ Server/index.js
@@ -0,0 +1,7 @@
+const adminserver = require("./Admin/server");
+const custserver = require("./Customer/server");
+const mechserver = require("./Mechanic/server");
+const orderserver = require("./Order/server");
+
+
+adminserver
